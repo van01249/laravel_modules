756f21240a926c01da6478e116aa0e5126df1b31
Van <phonglang01249@gmail.com>
2024-04-09 21:21:37 +0700

    Chinh sua seeder StudentBook

--- Modules/StudentBook/Database/Seeders/StudentBookDatabaseSeeder.php
+++ Modules/StudentBook/Database/Seeders/StudentBookDatabaseSeeder.php
@@ -16,6 +16,6 @@ class StudentBookDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->call(StudentBookTableSeeder::class);
     }
 }
